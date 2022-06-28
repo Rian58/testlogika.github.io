ad114ec387e0afa35f4c0f78ca59f3990031c8b5
Rian58 <57803745+Rian58@users.noreply.github.com>
2022-06-28 19:55:22 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+alert('Hallo selamat datang di website kami! ')
+
+var lagi = true;
+
+while( lagi === true ) {
+	var nama = prompt('Masukan Nama Kamu : ')
+	alert('Hallo ' + nama );
+
+	lagi = confirm('Coba Lagi?');
+}
+
+alert('Terima Kasih :)')
+
